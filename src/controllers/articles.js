345f797f385acf7019a70fd155443e6080a45b5a
Mukul <mukulkumar627@gmail.com>
2021-02-27 19:03:00 +0530

    Article controller function declaration added

---
+++ src/controllers/articles.js
@@ -0,0 +1,24 @@
+//String String String String[]
+async function createArticle(title, description, body, tagList) {
+
+}
+
+//String
+async function deleteArticle(slug) {}
+
+//String String String String String[]
+async function updateArticle(slug, title, description, body, tags) {}
+
+
+async function getAllArticles() {
+
+}
+
+//Personalised feed, need the email
+//String
+async function getFeedArticles(email) {
+
+}
+
+//String
+async function getArticleBySlug(slug) {}
