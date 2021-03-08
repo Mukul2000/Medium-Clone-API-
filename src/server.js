dc5071eacbbad58681bd5dc6772c90905cb08f6d
Mukul <mukulkumar627@gmail.com>
2021-03-08 20:17:44 +0530

    CORS error fix

--- src/server.js
+++ src/server.js
@@ -13,7 +13,14 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use("/api/articles", articleRoute);
 app.use("/api/user", userRoute);
 app.use("/api/users", usersRoute);
-app.use(cors());
+app.use(cors(), function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server started on port 3000...");
