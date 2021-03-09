83f2d85abff448d4b6e75d345741d9a80b6d24fe
Mukul <mukulkumar627@gmail.com>
2021-03-09 17:32:02 +0530

    Add cors package

--- src/server.js
+++ src/server.js
@@ -7,20 +7,13 @@ const userRoute = require('./routes/user');
 const articleRoute = require('./routes/articles');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // support json encoded bodies
 
 app.use("/api/articles", articleRoute);
 app.use("/api/user", userRoute);
 app.use("/api/users", usersRoute);
-app.use(cors(), function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server started on port 3000...");
